6ad069c1adf32a4135a914be6d0888be9775da36
Dirk Kok <the@dirkkok.nl>
2022-05-01 01:32:19 +0200

    initial commit

---
+++ RandomlyPingFishermanBot/Program.cs
@@ -0,0 +1,22 @@
+﻿using DSharpPlus;
+using DSharpPlus.Entities;
+
+string botToken = Environment.GetEnvironmentVariable("BOT_TOKEN")!;
+ulong targetId = ulong.Parse(Environment.GetEnvironmentVariable("TARGET_ID")!);
+ulong guildId = ulong.Parse(Environment.GetEnvironmentVariable("GUILD_ID")!);
+
+var random = new Random();
+var discord = new DiscordClient(new DiscordConfiguration() {
+	Token = botToken
+});
+
+await discord.ConnectAsync();
+
+while (true) {
+	await Task.Delay(TimeSpan.FromHours(random.NextDouble() * 12));
+	
+	DiscordGuild guild = await discord.GetGuildAsync(guildId);
+	List<DiscordChannel> channels = guild.Channels.Values.ToList();
+	DiscordChannel channel = channels[random.Next(0, channels.Count)];
+	await channel.SendMessageAsync($"<@{targetId}>");
+}
