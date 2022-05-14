04ae183e7658937005bbf8464fb044eb9a0ecbd8
Dirk Kok <the@dirkkok.nl>
2022-05-14 20:19:40 +0200

    pick random channel in any guild, and make sure the target can access the channel

--- RandomlyPingFishermanBot/Program.cs
+++ RandomlyPingFishermanBot/Program.cs
@@ -3,7 +3,6 @@ using DSharpPlus.Entities;
 
 string botToken = Environment.GetEnvironmentVariable("BOT_TOKEN")!;
 ulong targetId = ulong.Parse(Environment.GetEnvironmentVariable("TARGET_ID")!);
-ulong guildId = ulong.Parse(Environment.GetEnvironmentVariable("GUILD_ID")!);
 
 var random = new Random();
 var discord = new DiscordClient(new DiscordConfiguration() {
@@ -14,9 +13,22 @@ await discord.ConnectAsync();
 
 while (true) {
 	await Task.Delay(TimeSpan.FromHours(random.NextDouble() * 22 + 2));
-	
-	DiscordGuild guild = await discord.GetGuildAsync(guildId);
-	List<DiscordChannel> channels = guild.Channels.Values.ToList();
-	DiscordChannel channel = channels[random.Next(0, channels.Count)];
-	await channel.SendMessageAsync($"<@{targetId}>");
+
+	// List all channels in all guilds that the target can access (but not necessarily read history) and the bot can send messages in
+	var channels = new List<DiscordChannel>();
+	foreach (DiscordGuild guild in discord.Guilds.Values) {
+		DiscordMember? targetMember = await guild.GetMemberAsync(targetId);
+		if (targetMember != null) {
+			foreach (DiscordChannel channel in guild.Channels.Values) {
+				if ((channel.PermissionsFor(guild.CurrentMember) & Permissions.SendMessages) != 0 &&
+					(channel.PermissionsFor(targetMember) & Permissions.AccessChannels) != 0) {
+					channels.Add(channel);
+				}
+			}
+		}
+	}
+
+	// Crash if there aren't any channels
+	DiscordChannel selectedChannel = channels[random.Next(0, channels.Count)];
+	await selectedChannel.SendMessageAsync($"<@{targetId}>");
 }
